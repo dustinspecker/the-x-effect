1f49efc27be5efc7232dc570334af8827b8a0930
Dustin Specker <DustinSpecker@DustinSpecker.com>
2017-06-30 20:00:37 -0500

    feat(reducers): add handler for ADD_CARD

--- src/reducers/cards/index.js
+++ src/reducers/cards/index.js
@@ -1,1 +1,14 @@
-export default (state = []) => state
+import {ADD_CARD} from '../../actions/cards'
+
+export default (state = [], action = {}) => {
+  if (action.type === ADD_CARD) {
+    const {title} = action
+
+    return [
+      ...state,
+      { title }
+    ]
+  }
+
+  return state
+}
--- src/reducers/cards/index.test.js
+++ src/reducers/cards/index.test.js
@@ -1,8 +1,16 @@
+import {addCard} from '../../actions/cards/'
 import cardsReducer from './'
 
 describe('Cards Reducer', () => {
   it('default is empty array', () => {
     const state = cardsReducer()
     expect(state).toEqual([])
+  })
+
+  it('add card action', () => {
+    const state = [{title: 'yolo'}]
+    const nextState = cardsReducer(state, addCard('hey'))
+    const expectedState = [{title: 'yolo'}, { title: 'hey' }]
+    expect(nextState).toEqual(expectedState)
   })
 })
